36adcce0343a9348c96d17b32a59e9fef3820b1f
Çağıl Uluşahin <cagilulusahin@gmail.com>
2015-02-25 21:54:59 +0200

    crawler v1

--- main/crawler.py
+++ main/crawler.py
@@ -1,5 +1,6 @@
 from BeautifulSoup import BeautifulSoup
-import urllib2,codecs
+import codecs,traceback
+from urllib2 import HTTPError,urlopen
 """
 url = "http://www1.folha.uol.com.br/fsp/dinheiro/fi28079905.htm"
 url = "http://www1.folha.uol.com.br/fsp/cotidiano/153605-carnaval-do-rio-tera-parodia-de-marchinhas-em-tom-de-protesto.shtml"
@@ -14,12 +15,38 @@ def read_file_gen(filename):
                 break
             yield line
 
-def crawl(filename):
+def crawl(filename,pages):
     url_gen = read_file_gen(filename)
     for url in url_gen:
-        response = urllib2.urlopen(url)
-        page = BeautifulSoup(response.read())
         try:
-            page.findAll("td")[-2].findAll("p")[-1]
-        except IndexError:
-            page.find("div", {"id": "articleNew"})
+            response = urlopen(url)
+            page = BeautifulSoup(response.read())
+            if page:
+                pages.append(page)
+            else:
+                print("Empty page: %s" % url)
+        except HTTPError:
+            print(url)
+            traceback.print_exc()
+
+def parse_html(page):
+    try:
+        content = page.findAll("td")[-2].findAll("p")[-1]
+    except IndexError:
+        try:
+            content = page.find("div", {"id": "articleNew"})
+        except:
+            traceback.print_exc()
+            return None
+    return content
+
+def parse_pages(pages,training,test,label):
+    i = 0
+    for page in pages:
+        content = parse_html(page)
+        if page is not None:
+            if i<2:
+                test.append((content,label))
+            else:
+                training.append((content,label))
+            i = (i + 1) % 3
