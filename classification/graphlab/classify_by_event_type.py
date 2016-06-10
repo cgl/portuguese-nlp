e6187e45a5774916600b61c82639da799111c1e6
Cgl <cagilulusahin@gmail.com>
2016-06-10 20:44:47 +0300

    fields

--- classification/graphlab/classify_by_event_type.py
+++ classification/graphlab/classify_by_event_type.py
@@ -3,58 +3,58 @@ import argparse,codecs
 
 # result_dataset = gl.load_sframe("graphlab/my_dataset_test") # "my_dataset
 def save_positive_results_with_event_type_and_date(result_dataset):
-        csvfile = "classification/data/extraction_fields.tsv"
-        with codecs.open(csvfile,"r",encoding="utf8") as infile:
-            lines = infile.readlines()
-        #types = set()
-        #for line in lines[5:]:
-        #        types.add(line.split(",")[2].strip().lower())
-
-        types = {u'armed struggle' : 0,
+    csvfile = "classification/data/extraction_fields.tsv"
+    with codecs.open(csvfile,"r",encoding="utf8") as infile:
+        lines = infile.readlines()
+    #types = set()
+    #for line in lines[5:]:
+    #        types.add(line.split(",")[2].strip().lower())
+    types = {u'armed struggle' : 0,
                  u'invasion' : 1,
                  u'occupation': 2,
                  u'protest' : 3,
                  u'rebellion': 4,
                  u'strike' :5}
 
-        sf = gl.load_sframe("graphlab/my_training_dataset")
-        #for ind in range(0,521):
-        #    sf['filenames'][ind]
+    sf = gl.load_sframe("graphlab/my_training_dataset")
+    #for ind in range(0,521):
+    #    sf['filenames'][ind]
 
-        labels = [0]*521
+    labels = [0]*521
 
-        for line in lines[5:]:
+    for line in lines[5:]:
             fields = line.split("\t")
-            labels[int(fields[0].strip())] = types[fields[2].strip().lower()]
+            key = fields[2].strip().lower()
+            if key:
+                labels[int(fields[0].strip())] = types[key]
+    #for ind in range(0,521):
+    #    labels[int(sf['filenames'][ind][1:-4]))]
 
-        #for ind in range(0,521):
-        #    labels[int(sf['filenames'][ind][1:-4]))]
+    #rel_folder="classification/data/v5/class_rel/"
+    ef = sf.filter_by([1], "rel") # add_arguments(None,rel_folder,1,vec_model)
 
-        #rel_folder="classification/data/v5/class_rel/"
-        ef = sf.filter_by([1], "rel") # add_arguments(None,rel_folder,1,vec_model)
+    ef['event_type'] = ef['filenames'].apply(lambda p: labels[int(p[1:-4])])
 
-        ef['event_type'] = ef['filenames'].apply(lambda p: labels[int(p[1:-4])])
+    # evnt type classifier
+    event_type_cls = gl.classifier.create(ef, target="event_type",features=['vectors','1gram features'])
 
-        # evnt type classifier
-        event_type_cls = gl.classifier.create(ef, target="event_type",features=['vectors','1gram features'])
+    pos_results = result_dataset.filter_by([1], "rel")
 
-        pos_results = result_dataset.filter_by([1], "rel")
+    pos_res_res = event_type_cls.classify(pos_results)
 
-        pos_res_res = event_type_cls.classify(pos_results)
+    pos_results.add_column(pos_res_res.select_column("class"),"event_type")
+    pos_results.add_column(pos_res_res.select_column("probability"),"et_probability")
 
-        pos_results.add_column(pos_res_res.select_column("class"),"event_type")
-        pos_results.add_column(pos_res_res.select_column("probability"),"et_probability")
-
-        pos_results.filter_by([5],"event_type")
+    pos_results.filter_by([5],"event_type")
 
 
-        pos_results['date'] = pos_results['filenames'].apply(lambda x: x[:-5].split('_'))
-        pos_results = pos_results.unpack('date')
-        pos_results.rename({'date.0':'year', 'date.1':'month','date.2':'day', 'date.3':'index'})
+    pos_results['date'] = pos_results['filenames'].apply(lambda x: x[:-5].split('_'))
+    pos_results = pos_results.unpack('date')
+    pos_results.rename({'date.0':'year', 'date.1':'month','date.2':'day', 'date.3':'index'})
 
-        pos_results.save("graphlab/pos_results") ##_2005")
+    pos_results.save("graphlab/pos_results") ##_2005")
 
-        #month_count = pos_results.groupby(['year','month'], gl.aggregate.COUNT)
+    #month_count = pos_results.groupby(['year','month'], gl.aggregate.COUNT)
 
 def by_year_month(pos_results):
     sframe = gl.SFrame()
