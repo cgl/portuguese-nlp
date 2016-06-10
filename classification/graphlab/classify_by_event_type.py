fb291d3e15aea4d6dc3cad66ca3f5be241d2f8b2
Cgl <cagilulusahin@gmail.com>
2016-06-10 19:02:00 +0300

    fx

--- classification/graphlab/classify_by_event_type.py
+++ classification/graphlab/classify_by_event_type.py
@@ -81,8 +81,7 @@ def main():
     parser = argparse.ArgumentParser(description = "Classifies given dataset and saves the results.")
     parser.add_argument("--classified_dir", required = True, default=None ,type=str,
                         help = "Directory for dataset after classification ex: result_dataset")
-    #parser.add_argument("--print", required = False ,action='store_true', dest='print_results',
-                        help = "")
+    #parser.add_argument("--print", required = False ,action='store_true', dest='print_results',help = "")
 
     args = parser.parse_args()
     if args.classified_dir:
